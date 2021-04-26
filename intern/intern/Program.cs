d53f9bfeb9bafb721200ab201be6caab3ea993b0
Enes Tuğberk Kılıç <43927255+enesraf@users.noreply.github.com>
2021-04-26 15:53:32 +0300

    Add files via upload

---
+++ intern/intern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace intern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Please give a number as 1st variable: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please give a number as 2st variable: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.Write("The value of the 1st variable: {0} \n", a);
+
+            Console.Write("The value of the 1st variable: {0}", b);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
